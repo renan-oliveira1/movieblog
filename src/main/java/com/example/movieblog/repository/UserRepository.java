c47687cc4b0f7440cb9b939b2481c41481dfb4a9
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-05-23 19:01:17 -0300

    create user repository

---
+++ src/main/java/com/example/movieblog/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.movieblog.repository;
+
+import com.example.movieblog.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    UserDetails findByEmail(String email);
+}

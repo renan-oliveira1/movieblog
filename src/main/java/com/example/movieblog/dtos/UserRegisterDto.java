e2049d3f9222d6128296ad79a3923c777ee15085
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-06-12 10:11:22 -0300

    Validate if the information of authority is null

--- src/main/java/com/example/movieblog/dtos/UserRegisterDto.java
+++ src/main/java/com/example/movieblog/dtos/UserRegisterDto.java
@@ -1,6 +1,7 @@
 package com.example.movieblog.dtos;
 
 import com.example.movieblog.models.UserAuthority;
+import org.springframework.lang.NonNull;
 
-public record UserRegisterDto(String username, String email, String password, String phone, UserAuthority authority) {
+public record UserRegisterDto(String username, String email, String password, String phone, @NonNull UserAuthority authority) {
 }
